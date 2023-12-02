e5061a8dff99a5d93bd9e61ac8d45bece5abf2c9
Alexander Liebald <alexander.liebald@gmail.com>
2023-12-02 19:15:00 +0100

    Basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
+import { Container, Title, Text } from "@mantine/core";
 import styles from "./page.module.css";
+import Anchor from "@/atoms/anchor/anchor";
 
 export default function Home() {
   return (
-    <div>
-      Landing Page
-      <br />
-      About
-      <br />
-      Help / HowTo
-    </div>
+    <Container>
+      <Title size="h1" order={1}>
+        Social Games Finder
+      </Title>
+      <Text pt="md">
+        This website provides a collection of various mostly free social games.
+      </Text>
+
+      <Title size="h1" order={2} pt="xl">
+        Features
+      </Title>
+      <Text pt="md">
+        This site is still in early development. At the moment, you can view a
+        games list <Anchor href="/games">here</Anchor>
+      </Text>
+    </Container>
   );
 }
