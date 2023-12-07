767bb443376e6261003eecd430eec6c9f10d835b
Alexander Liebald <alexander.liebald@gmail.com>
2023-12-07 21:14:24 +0100

    Mantine update & fix mismatched dependency versions

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,21 @@
 import { createTheme } from "@mantine/core";
 
 const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    // Use pre 7.3.0 dark colors. See https://github.com/mantinedev/mantine/releases/tag/7.3.0
+    dark: [
+      "#C1C2C5",
+      "#A6A7AB",
+      "#909296",
+      "#5c5f66",
+      "#373A40",
+      "#2C2E33",
+      "#25262b",
+      "#1A1B1E",
+      "#141517",
+      "#101113",
+    ],
+  },
 });
 
 export default theme;
