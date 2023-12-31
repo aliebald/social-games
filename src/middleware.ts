0869a69d1e5bf9b99ec5d8314214bd99d7a91fe3
Alexander Liebald <alexander.liebald@gmail.com>
2023-12-31 17:26:47 +0100

    Add optional basic auth

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/:path*",
+};
+
+export function middleware(req: NextRequest) {
+  console.log(typeof process.env.BASIC_AUTH_USER);
+
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  const validUser = process.env.BASIC_AUTH_USER;
+  const validPassWord = process.env.BASIC_AUTH_PASSWORD;
+  const doesNotUseBasicAuth = validUser === "" && validPassWord === "";
+
+  if (doesNotUseBasicAuth) {
+    return NextResponse.next();
+  }
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (user === validUser && pwd === validPassWord) {
+      return NextResponse.next();
+    }
+  }
+
+  url.pathname = "/api/auth";
+
+  return NextResponse.rewrite(url);
+}
