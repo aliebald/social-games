6a36ce3299b4141d69e27e97d428c79a508a2c6f
Alexander Liebald <alexander.liebald@gmail.com>
2024-08-16 09:46:57 +0200

    optimize mantine package imports

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
 	publicRuntimeConfig: {
 		lastModifiedYear: new Date().getFullYear(),
 	},
+	experimental: {
+		optimizePackageImports: ['@mantine/core', '@mantine/form', '@mantine/hooks', '@mantine/notifications'],
+	},
 };
 
 module.exports = nextConfig;
